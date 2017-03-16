d843ee8f3aae672f19e45112419e2c3438d565e7
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-03-15 20:19:06 -0700

    Create PromptsAlertsConfirms.js

---
+++ PromptsAlertsConfirms.js
@@ -0,0 +1,10 @@
+alert("Alert box1", "Alert box2");
+confirm("Confirm 1", "Confirm2");
+prompt("how are you?", "I am well, thank you");
+
+
+//ALERT accepts only 1 parameter
+//CONFIRM accepts only 1 parameter
+//PROMPT accepts 2 parameters
+//parameter is a set of answers in a function that it can accept. 
+//above code has 2 params for ALERT and CONFIRM functions. 2nd param does not work. JS ignores 2nd set of params
