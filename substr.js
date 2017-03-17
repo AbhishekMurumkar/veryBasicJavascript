36245d420938a24edb48e5571188b0abf9235d24
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-03-16 23:34:10 -0700

    Update substr.js

--- substr.js
+++ substr.js
@@ -1,2 +1,6 @@
 var myString = "My favorite book is Black Tullip";
 document.write(myString.substr(0,20) + "The Count of Monte Cristo");
+
+
+var myString1 = "I love Google";
+document.write("I live on " + myString1.substr(6));
