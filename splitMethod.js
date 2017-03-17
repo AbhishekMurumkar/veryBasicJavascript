fc1fe64ac46de9bd1b7adc476198510701d2f798
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-03-16 23:06:50 -0700

    Update splitMethod.js

--- splitMethod.js
+++ splitMethod.js
@@ -1,7 +1,20 @@
 var myString = "Amur tigers are endangered.";
-var splitString = myString.split(" ");
+var splitString = myString.split("");
+
+var splitString = myString.split("");
 document.write("Here is the array: <br />");
 
 for (index in splitString) {
      document.write(splitString [index] + " , ");
+     }
+
+document.write("<br />");
+
+
+var myString1 = "Aurora Borealis is beautiful";
+var splitString1 = myString1.split(" ");
+document.write("Here is 2nd array: <br />");
+
+for (index1 in splitString1) {
+     document.write(splitString1 [index1] + " , ");
      }
