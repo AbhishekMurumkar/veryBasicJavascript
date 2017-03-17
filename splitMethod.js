33cff88b017f1f75cbb051ea238ed4e1811a9cff
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-03-16 22:59:53 -0700

    Create splitMethod.js

---
+++ splitMethod.js
@@ -0,0 +1,7 @@
+var myString = "Amur tigers are endangered.";
+var splitString = myString.split(" ");
+document.write("Here is the array: <br />");
+
+for (index in splitString) {
+     document.write(splitString [index] + " , ");
+     }
