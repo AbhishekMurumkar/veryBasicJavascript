15879a36abfb838ddc2c35a2ea46b44842bc8b8e
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-03-19 00:20:55 -0700

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,3 +1,19 @@
+var person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 47
+};
+
+localStorage.person = JSON.stringify(person);
+var personObj=JSON.parse(localStorage.person);
+alert (personObj.firstName+ " " + personObj.lastName);
+localStorage.clear();
+
+
+
+
+
+/*
 //not supported by IE7
 //local storage object
 //holds name and value
@@ -8,3 +24,17 @@ localStorage.setItem("lastName", "Johnson");
 
 alert(localStorage.firstName + " " + localStorage.getItem("lastName"));
 
+localStorage.removeItem("lastName");
+alert (localStorage.lastName);
+
+localStorage.clear();
+alert(localStorage.firstName);
+//undefined - firstname is no longer there
+
+localStorage.age=33;
+//will not work needs to be converted into a int
+
+var age = parseInt(localStorage.age, 10);
+
+alert(typeof localStorage.age);
+*/
