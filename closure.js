2c974ea601b83c4e0b5c2db58ee2a490556fe309
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-03-18 19:31:18 -0700

    Update closure.js

--- closure.js
+++ closure.js
@@ -6,3 +6,19 @@ function doSomething (arg1) {
 
 var fn = doSomething("hello, this is a closure");
 fn();
+
+
+
+function doSomething (arg1) {
+  return function() {
+    alert(arg1);
+  };
+}
+
+var fn = doSomething("hello, this is a closure");
+fn();
+
+var fn1 = doSomething("hello, this is closure 2");
+fn1();
+
+fn();
