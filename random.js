64434ea7293199b51f599551d4747d994f18804a
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-03-18 17:51:19 -0700

    Create random.js

---
+++ random.js
@@ -0,0 +1,7 @@
+var value = Math.random();
+alert(value);
+
+var value1 = Math.floor(Math.random()*11);
+alert(value1);
+//let's say we need a random number between 1 and 10 ... floor() gives us a whole number
+//multiplication creates a range between 0 and 11
