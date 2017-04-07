6ecd19dc1a8031e2abc3f9ffc09d491908465afc
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-04-07 04:57:06 -0700

    Create === example.js

---
+++ === example.js
@@ -0,0 +1,13 @@
+
+var color = "brown-red";
+
+function changeColor() {
+  if (color === "red") {
+    alert (color + " is the color");
+    color = "pink";
+  } else {
+    alert (color);
+  }
+}
+
+changeColor();
