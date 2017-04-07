4da8b6d3b7c26ef1e7fd880ce4298e069251e597
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-04-07 05:08:12 -0700

    Create objLiteralNotation.js

---
+++ objLiteralNotation.js
@@ -0,0 +1,12 @@
+
+//object literal notation
+
+var person = {
+  name: "Nicholas",
+  age: 30,
+  city: "Amsterdam",
+  country: "NL"
+}
+
+alert (person.name);
+alert (person.name + " is from " + person.city);
