fbb4ad5e4b71699f2e674185b4f90eeb7605a0cb
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-04-07 04:38:34 -0700

    Create sillyJS.js
    very very silly js scripts

---
+++ sillyJS.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+/*
+var Hello = "Hello";
+var Goodbye = "good bye";
+
+if (Hello < Goodbye) {
+  alert (Goodbye);
+} else {alert (Hello);}
+
+*/
+
+/*
+var hello = "hello";
+var goodbye = "good bye";
+
+if (hello < goodbye) {
+  alert (goodbye);
+} else {alert (hello);}
+*/
+
+/*
+var lang = "Java";
+var lang = lang + "script";
+alert(lang);
+*/
+
+/*
+var text = "We are living in Amerika, Amerika das wunderbar";
+alert(text.length);
+*/
+
+
+/*
+function myFirstFunction(){
+  var hiThere1="Hi there";
+  alert(hiThere1);
+}
+myFirstFunction();
+*/
+
+/*
+function test(){
+  message = "this is global";
+}
+test();
+alert(message);
+*/
+
+/*
+var message;
+if (message) {
+  alert("the value is true");
+}
+else { alert ("no message");}
+*/
+
