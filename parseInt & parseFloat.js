7f90148102235760057be677a60146b1c1875a7e
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-04-09 00:36:49 -0700

    Create parseInt & parseFloat.js

---
+++ parseInt & parseFloat.js
@@ -0,0 +1,14 @@
+var num = parseInt('22224.342');
+//console.log(num);
+console.log(typeof(num));
+
+var num = parseInt('124.89');
+console.log(num);
+
+var num = parseInt('fff333', 16);
+console.log(num);
+//typeof(num);
+
+
+var decimal22 = parseFloat("1.92929");
+console.log (decimal22);
