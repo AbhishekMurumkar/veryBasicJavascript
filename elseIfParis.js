863fea845bcafb4293708a5d61e1d30c0d02e95f
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-04-16 00:15:58 -0700

    Create elseIfParis.js

---
+++ elseIfParis.js
@@ -0,0 +1,10 @@
+var answer = prompt ("What is the capital of France");
+
+if (answer === "Paris" || answer === "paris")
+  {
+    alert("you are correct");
+  }
+else 
+  {
+alert (answer + " is wrong");
+  }
