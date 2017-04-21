765c4d73298cdd41da1a326af59ffb0a8ba4d492
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-04-20 18:54:01 -0700

    Create moreConcat.js

---
+++ moreConcat.js
@@ -0,0 +1,8 @@
+var greetingString = "Good Afternoon";
+var myName = prompt ("Please enter your name", " ");
+var myName = myName.toUpperCase();
+
+var concatString = greetingString + ", " + myName;
+alert (concatString);
+
+document.write(concatString);
