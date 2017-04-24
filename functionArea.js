ca792de24837a667bf1dd3a133a64ba09683449a
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-04-23 22:44:50 -0700

    Create functionArea.js

---
+++ functionArea.js
@@ -0,0 +1,15 @@
+function calculateSquare (side) {
+  console.log(side*side);
+}
+calculateSquare(19)
+
+function calculateArea (side1, side2){
+  console.log(side1*side2);
+}
+calculateArea(5,5);
+
+
+function randomNum(){
+  console.log(Math.random()*100);
+}
+randomNum();
