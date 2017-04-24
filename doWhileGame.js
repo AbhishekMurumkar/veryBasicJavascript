d671587a3ba75fe22b43dca09d5c41f8394966c7
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2017-04-23 21:43:18 -0700

    Create doWhileGame.js

---
+++ doWhileGame.js
@@ -0,0 +1,15 @@
+var sad = confirm ("Would you like to be cheered up?");
+
+while (sad){
+alert ("What do you call a boomerang that doesn't come back? ... A stick");
+  
+var yes = confirm ("Tell me about your feelings?", "Type your feelings here");
+
+if (response) {
+alert("I am sorry you dont feel better");
+sad = true;
+}
+else {
+sad = false;
+}
+}
