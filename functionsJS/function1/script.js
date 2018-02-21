be342438e27a2b4acc99a619efec6c0677da0a66
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2018-02-20 21:04:17 -0800

    Create script.js

---
+++ functionsJS/function1/script.js
@@ -0,0 +1,18 @@
+//how to write a function
+//defined with the function keyword
+//functions have to be declared
+
+function plus(a , b) {
+//(param1, param2)
+//they are avail within the function 
+//{} function statement goes here
+
+var sum = a + b;
+//include return statement 
+return sum;
+
+}
+
+//invoke a function
+console.log(plus(3,5));
+plus(3,5);
