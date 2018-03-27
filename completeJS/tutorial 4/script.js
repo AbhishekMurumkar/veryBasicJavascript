7f74398b5a470e948aea2dcbeabe51bfc9dcee61
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2018-03-26 19:33:51 -0700

    Add files via upload

---
+++ completeJS/tutorial 4/script.js
@@ -0,0 +1,8 @@
+var a = 30;
+//we assign value of 30 to a variable a
+
+function print(myName, age, city){
+    alert("My name is " + myName + " .I am from " + city + " . I am " + age);
+}
+
+print("Nataly", 23, "Paris");
