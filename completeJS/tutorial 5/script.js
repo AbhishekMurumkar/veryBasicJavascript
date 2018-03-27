ed609b1e01a5a1f2885dddcd6f5200154a3c1933
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2018-03-26 19:38:19 -0700

    Add files via upload

---
+++ completeJS/tutorial 5/script.js
@@ -0,0 +1,8 @@
+var a = 30;
+//we assign value of 30 to a variable a
+
+var intro = function (myName, age, city){
+    alert("My name is " + myName + " .I am from " + city + " . I am " + age);
+}
+
+intro("Nataly", 23, "Paris");
