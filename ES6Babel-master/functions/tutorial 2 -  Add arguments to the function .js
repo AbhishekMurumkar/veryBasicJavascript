dd55b7dd4c080af3af1ac9c97876d83aa1fef5f9
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2018-04-13 23:05:10 -0700

    Create tutorial 2 -  Add arguments to the function .js

---
+++ ES6Babel-master/functions/tutorial 2 -  Add arguments to the function .js
@@ -0,0 +1,22 @@
+function findBiggestFractions(a,b){
+a > b ? console.log("a: ",a) :
+console.log ("b : ", b);
+}
+
+var firstFraction = 3/4;
+var secondFraction = 5/7;
+
+findBiggestFractions(firstFraction, secondFraction);
+
+findBiggestFractions(4/4, 6/4);
+
+
+//
+"a: "
+
+0.75
+
+"b : "
+
+1.5
+//
