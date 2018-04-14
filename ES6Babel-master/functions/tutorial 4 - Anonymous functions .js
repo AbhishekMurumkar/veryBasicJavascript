adb17d0592a925648aa480df1c9b6aec744c93ad
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2018-04-13 23:42:57 -0700

    Update tutorial 4 - Anonymous functions .js

--- ES6Babel-master/functions/tutorial 4 - Anonymous functions .js
+++ ES6Babel-master/functions/tutorial 4 - Anonymous functions .js
@@ -11,3 +11,18 @@ console.log(result);
 theBiggest();
 
 //inside of theBiggest variable is a function
+
+
+//if you are using return you need to add console.log()
+
+var a = 5/8;
+var b = 4/7;
+
+
+var theBiggest = function (){
+var result;
+a>b ? result = ["a", a] : result =["b", b];
+return result;
+}
+
+console.log(theBiggest());
